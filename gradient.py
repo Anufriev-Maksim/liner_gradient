d2d7b21b38b6d258a31de5af362b8dcb89d069c1
Anufriev Maksim <74050295+Anufriev-Maksim@users.noreply.github.com>
2021-11-19 12:45:47 +0800

    Update gradient.py

--- gradient.py
+++ gradient.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 100, 255, dtype=int)
-y = np.linspace(0, 100, 255, dtype=int)
+size = 300
+
+x = np.linspace(0, 100, size, dtype=int)
+y = np.linspace(0, 100, size, dtype=int)
 
 arr_x, arr_y = np.meshgrid(x, y)
 plotArray = arr_x + arr_y
